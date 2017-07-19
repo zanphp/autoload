900fcc03f87a818ae0f39d94b525c416e7fbcae7
chuxiaofeng <chuxiaofeng@youzan.com>
2017-07-19 13:23:44 +0800

    ~

--- tests/main.php
+++ tests/main.php
@@ -3,6 +3,6 @@
 require __DIR__ . "/vendor/autoload.php";
 
 
-//class_exists(\ZanPHP\Autoload\Tests\StaticTest::class, true);
+class_exists(\ZanPHP\Autoload\Tests\StaticTest::class, true);
 
 class_exists(\ZanPHP\Autoload\Tests\StaticInterfaceTest::class, true);
